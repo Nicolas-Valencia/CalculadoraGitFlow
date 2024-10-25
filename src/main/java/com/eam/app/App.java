ff1b861e094384d8683a65e31ff33168e4124988
Diyued <diegorinconch2607@gmail.com>
2024-10-25 11:06:47 -0500

    ADD sumar y restar

---
+++ src/main/java/com/eam/app/App.java
@@ -0,0 +1,12 @@
+package main.java.com.eam.app;
+
+public class App {
+	
+	public static double sumar(double a, double b) {
+		return a+b;
+	}
+	public static double restar(double a, double b) {
+		return a-b;
+	}
+	
+}
